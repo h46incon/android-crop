3c9efc020edef124fefd89833ecc9818305c6988
h46incon <h46incon@gmail.com>
2014-09-02 22:36:57 +0800

    Implement text placer, ugly codes, and with baseline bug

--- example/src/main/java/com/soundcloud/android/crop/example/TextPlacerActivity.java
+++ example/src/main/java/com/soundcloud/android/crop/example/TextPlacerActivity.java
@@ -1,9 +1,11 @@
 package com.soundcloud.android.crop.example;
 
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 
 import com.soundcloud.android.crop.CropImageView;
@@ -15,14 +17,20 @@ import com.soundcloud.android.crop.ImageAreaPickerActivity;
  * Created by h46incon on 2014/9/2.
  */
 public class TextPlacerActivity extends ImageAreaPickerActivity{
-    CropImageView imageView;
-    HighlightView pickerView;
+    private CropImageView imageView;
+    private HighlightView pickerView;
+    private final String text = "Text-g";
+    private float initTextSize;
+    private Rect initTextBounds;
+    private float initTextAspectRatio;
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placer);
         initViews();
+        getInitTextBounds();
 
         // Check if the background is available
         if (rotateBitmap == null) {
@@ -30,7 +38,25 @@ public class TextPlacerActivity extends ImageAreaPickerActivity{
             return;
         }
 
-        setupPickerView(imageView);
+        float width = 1;
+        float height = width * initTextAspectRatio;
+
+        if (height > 1) {
+            height = 1;
+            width = height / initTextAspectRatio;
+        }
+
+        final float part = 0.4f;
+        width *= part;
+        height *= part;
+        RectF area = new RectF(0, 0, width, height);
+        area.offset(
+                0.5f - area.centerX(),
+                0.5f - area.centerY()
+        );
+
+
+        setupPickerView(imageView, area);
         // Configure highlight view
         pickerView = getPickerView();
         pickerView.setNeedCenterBaseOnThis(false);
@@ -44,7 +70,21 @@ public class TextPlacerActivity extends ImageAreaPickerActivity{
             @Override
             public void onDrawFinished(HighlightView v, Canvas canvas) {
                 Rect vArea = v.getCropRectOnScreen();
-                canvas.drawRect(vArea, paint);
+
+                float yRation = vArea.height() / (float) initTextBounds.height();
+                float newTextSize = initTextSize * yRation;
+
+                float xTemp = vArea.width() / yRation;
+                float xScale = xTemp / initTextBounds.width();
+
+                paint.setTextSize(newTextSize);
+                paint.setTextScaleX(xScale);
+
+//                Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
+                Rect targetRect = vArea;
+//                int baseline = targetRect.top + (targetRect.bottom - targetRect.top - fontMetrics.bottom + fontMetrics.top) / 2 - fontMetrics.top;
+//                canvas.drawRect(vArea, paint);
+                canvas.drawText(text, (float)vArea.left, (float)vArea.bottom, paint);
             }
         });
 
@@ -52,7 +92,17 @@ public class TextPlacerActivity extends ImageAreaPickerActivity{
         startPicker();
     }
 
+    private void getInitTextBounds() {
+        initTextSize = 300;
+        Paint paint = new Paint();
+        paint.setTextSize(initTextSize);
+        initTextBounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), initTextBounds);
+        initTextAspectRatio = initTextBounds.height() / (float) initTextBounds.width();
+    }
+
     void initViews() {
+        resources = getResources();
         imageView = (CropImageView) findViewById(R.id.crop_image_view);
     }
 
