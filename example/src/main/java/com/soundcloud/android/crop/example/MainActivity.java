9781f1d94da70be944ed69e4c9bafbe387b1ce22
h46incon <h46incon@gmail.com>
2014-09-02 16:55:27 +0800

    Add an item in menu to start TextPlacerActivity,

--- example/src/main/java/com/soundcloud/android/crop/example/MainActivity.java
+++ example/src/main/java/com/soundcloud/android/crop/example/MainActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,6 +16,8 @@ import java.io.File;
 public class MainActivity extends Activity {
 
     private ImageView resultView;
+    private static final int ID_PICK_TOTEXT = 0x333;
+    private static final int ID_TEXT_PLACER = 0x928;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +38,10 @@ public class MainActivity extends Activity {
             resultView.setImageDrawable(null);
             Crop.pickImage(this);
             return true;
+        } else if (item.getItemId() == R.id.action_placer_text) {
+            resultView.setImageDrawable(null);
+            Crop.pickImage(this, ID_PICK_TOTEXT);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -45,6 +50,10 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent result) {
         if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
             beginCrop(result.getData());
+        } else if (requestCode == ID_PICK_TOTEXT && resultCode == RESULT_OK) {
+            Intent intent = new Intent(this, TextPlacerActivity.class);
+            intent.setData(result.getData());
+            startActivityForResult(intent, ID_TEXT_PLACER);
         } else if (requestCode == Crop.REQUEST_CROP) {
             handleCrop(resultCode, result);
         }
